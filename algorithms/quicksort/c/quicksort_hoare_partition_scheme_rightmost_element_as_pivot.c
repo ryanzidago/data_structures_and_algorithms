591f9213ea7b19aab256f4749d7f0451db00dc1e
Ryan Zidago <ryanzidago@gmail.com>
2021-03-31 21:18:25 +0200

    updated quicksort implementation in c

--- algorithms/quicksort/c/quicksort_hoare_partition_scheme_rightmost_element_as_pivot.c
+++ algorithms/quicksort/c/quicksort_hoare_partition_scheme_rightmost_element_as_pivot.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void quicksort(int array[], int lenght);
+void quicksort(int array[]);
 void _quicksort(int array[], int left_index, int right_index);
 int partition(int array[], int left_index, int right_index);
 void swap(int array[], int i, int j);
@@ -8,15 +8,22 @@ void swap(int array[], int i, int j);
 int main(void) {
   int array[6] = {0, 5, 2, 6, 1, 3};
   int length = sizeof(array) / sizeof(array[0]);
-  quicksort(array, length);
+  
+  quicksort(array);
 
   for (int i = 0; i < length; i++) {
     printf("%d\n", array[i]);
   }
 }
 
-void quicksort(int array[], int length) {
+void quicksort(int *array) {
   int left_index = 0;
+
+  int length = 0;
+  while (array[length] != NULL) {
+    length += 1;
+  }
+
   int right_index = length - 1;
 
   _quicksort(array, left_index, right_index);
