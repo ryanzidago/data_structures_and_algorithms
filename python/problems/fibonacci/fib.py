737745a00ea22b08adf92c5ccd9c5e143f04c32b
Ryan Zidago <ryanzidago@gmail.com>
2021-05-08 16:48:21 +0200

    cleaned up code

--- python/problems/fibonacci/fib.py
+++ python/problems/fibonacci/fib.py
@@ -6,18 +6,22 @@ def fib1(n: int) -> int:
     return fib1(n - 1) + fib2(n - 2)
 
 # recursive
+# O(2^N)
 def fib2(n: int) -> int:
     if n < 2:
         return n
     return fib2(n - 1) + fib2(n - 2)
 
 # memoization 
+# O(N)
 def fib3(n: int, memo: Dict[int, int] = {0: 0, 1: 1}) -> int:
+    print(memo)
     if n not in memo:
         memo[n] = fib3(n - 1) + fib3(n - 2) # memoization 
     return memo[n]
 
 # LRU cachce
+# O(N)
 @lru_cache(maxsize=None)
 def fib4(n: int) -> int:
     if n < 2:
@@ -25,6 +29,7 @@ def fib4(n: int) -> int:
     return fib4(n - 1) + fib4(n - 2)
 
 # iterative
+# O(N)
 def fib5(n):
     if n == 0: return n
     last = 0 
@@ -33,7 +38,7 @@ def fib5(n):
         last, next = next, last + next 
     return next
 
-# yielding all vlalues
+# yielding all values
 def fib6(n):
     yield 0
     if n > 0:
@@ -43,5 +48,3 @@ def fib6(n):
     for _ in range(1, n):
         last, next = next, last + next 
         yield next
-
-print(fib5(5))
