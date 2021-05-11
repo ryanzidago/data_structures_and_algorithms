9fd9f2efe7e5f0fe2090e5963bc0adb666355342
Ryan Zidago <ryanzidago@gmail.com>
2021-05-11 20:32:21 +0200

    added decreasing insertion sort implementation in Python

--- python/algorithms/insertion-sort/insertion_sort.py
+++ python/algorithms/insertion-sort/insertion_sort.py
@@ -13,12 +13,25 @@ def insertion_sort(a):
             a[j - 1], a[j] = a[j], a[j - 1]
             j -= 1
 
-# test 
+def decreasing_insertion_sort(a):
+    for i in range(1, len(a)):
+        j = i
+        while j > 0 and a[j - 1 ] < a[j]:
+           a[j - 1], a[j] = a[j], a[j - 1]
+           j -= 1
+
+# test insertion_sort
 a = [5, 2, 4, 6, 1, 3]
 insertion_sort(a)
 assert a == [1, 2, 3, 4, 5, 6]
 
-# test 
+# test insertion_sort
 a = [4, 2, 7, 1, 3]
 insertion_sort(a)
 assert a == [1, 2, 3, 4, 7]
+
+# test decreasing_insertion_sort 
+a = [5, 2, 4, 6, 1, 3]
+decreasing_insertion_sort(a)
+assert a == [6, 5, 4, 3, 2, 1]
+
