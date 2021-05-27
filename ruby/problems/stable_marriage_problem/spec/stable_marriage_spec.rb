134247fab1e0adece2b5c5402be31e77886aeece
Ryan Zidago <ryanzidago@gmail.com>
2021-05-27 21:21:31 +0200

    added Rosetta Code's test case

--- ruby/problems/stable_marriage_problem/spec/stable_marriage_spec.rb
+++ ruby/problems/stable_marriage_problem/spec/stable_marriage_spec.rb
@@ -61,6 +61,101 @@ RSpec.describe Marriage do
 
             expect(e.engaged_to).to eq(l.name)
             expect(l.engaged_to).to eq(e.name)
+
+            abe = Man.new("abe")
+            bob = Man.new("bob")
+            col = Man.new("col")
+            dan = Man.new("dan")
+            ed = Man.new("ed")
+            fred = Man.new("fred")
+            gav = Man.new("gav")
+            hal = Man.new("hal")
+            ian = Man.new("ian")
+            jon = Man.new("jon")
+
+            men = {
+                "abe" => abe,
+                "bob" => bob,
+                "col" => col,
+                "dan" => dan,
+                "ed" => ed,
+                "fred" => fred,
+                "gav" => gav,
+                "hal" => hal,
+                "ian" => ian,
+                "jon" => jon,
+            }
+
+
+            abi = Woman.new("abi")
+            bea = Woman.new("bea")
+            cath = Woman.new("cath")
+            dee = Woman.new("dee")
+            eve = Woman.new("eve")
+            fay = Woman.new("fay")
+            gay = Woman.new("gay")
+            hope = Woman.new("hope")
+            ivy = Woman.new("ivy")
+            jan = Woman.new("jan")
+
+            women = {
+                "abi" => abi,
+                "bea" => bea,
+                "cath" => cath,
+                "dee" => dee,
+                "eve" => eve,
+                "fay" => fay,
+                "gay" => gay,
+                "hope" => hope,
+                "ivy" => ivy,
+                "jan" => jan,
+            }
+
+            abe.add_preferences([abi, eve, cath, ivy, jan, dee, fay, bea, hope, gay])
+            bob.add_preferences([cath, hope, abi, dee, eve, fay, bea, jan, ivy, gay])
+            col.add_preferences([hope, eve, abi, dee, bea, fay, ivy, gay, cath, jan])
+            dan.add_preferences([ivy, fay, dee, gay, hope, eve, jan, bea, cath, abi])
+            ed.add_preferences([jan, dee, bea, cath, fay, eve, abi, ivy, hope, gay])
+            fred.add_preferences([bea, abi, dee, gay, eve, ivy, cath, jan, hope, fay])
+            gav.add_preferences([gay, eve, ivy, bea, cath, abi, dee, hope, jan, fay])
+            hal.add_preferences([abi, eve, hope, fay, ivy, cath, jan, bea, gay, dee])
+            ian.add_preferences([hope, cath, dee, gay, bea, abi, fay, ivy, jan, eve])
+            jon.add_preferences([abi, fay, jan, gay, eve, bea, dee, cath, ivy, hope])
+
+            abi.add_preferences([bob, fred, jon, gav, ian, abe, dan, ed, col, hal])
+            bea.add_preferences([bob, abe, col, fred, gav, dan, ian, ed, jon, hal])
+            cath.add_preferences([fred, bob, ed, gav, hal, col, ian, abe, dan, jon])
+            dee.add_preferences([fred, jon, col, abe, ian, hal, gav, dan, bob, ed])
+            eve.add_preferences([jon, hal, fred, dan, abe, gav, col, ed, ian, bob])
+            fay.add_preferences([bob, abe, ed, ian, jon, dan, fred, gav, col, hal])
+            gay.add_preferences([jon, gav, hal, fred, bob, abe, col, ed, dan, ian])
+            hope.add_preferences([gav, jon, bob, abe, ian, dan, hal, ed, col, fred])
+            ivy.add_preferences([ian, col, hal, gav, fred, bob, abe, ed, jon, dan])
+            jan.add_preferences([ed, hal, gav, abe, bob, jon, col, ian, fred, dan])
+
+            Marriage.stable_match(men, women)
+
+            expect(abe.engaged_to).to eq(ivy.name)
+            expect(bob.engaged_to).to eq(cath.name)
+            expect(col.engaged_to).to eq(dee.name)
+            expect(dan.engaged_to).to eq(fay.name)
+            expect(ed.engaged_to).to eq(jan.name)
+            expect(fred.engaged_to).to eq(bea.name)
+            expect(gav.engaged_to).to eq(gay.name)
+            expect(hal.engaged_to).to eq(eve.name)
+            expect(ian.engaged_to).to eq(hope.name)
+            expect(jon.engaged_to).to eq(abi.name)
+
+            expect(ivy.engaged_to).to eq(abe.name)
+            expect(cath.engaged_to).to eq(bob.name)
+            expect(dee.engaged_to).to eq(col.name)
+            expect(fay.engaged_to).to eq(dan.name)
+            expect(jan.engaged_to).to eq(ed.name)
+            expect(bea.engaged_to).to eq(fred.name)
+            expect(gay.engaged_to).to eq(gav.name)
+            expect(eve.engaged_to).to eq(hal.name)
+            expect(hope.engaged_to).to eq(ian.name)
+            expect(abi.engaged_to).to eq(jon.name)
         end
     end
 end
