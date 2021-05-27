bb2cd309b1434d63cdf3f4c97d6452a7a322af36
Ryan Zidago <ryanzidago@gmail.com>
2021-05-28 00:36:27 +0200

    finished solving the stable marriage problem

--- elixir/problems/stable_marriage/lib/stable_marriage.ex
+++ elixir/problems/stable_marriage/lib/stable_marriage.ex
@@ -1,42 +1,70 @@
 defmodule StableMarriage do
-  def match(men, women, state \\ %{}, is_stable \\ false)
+  @type man() :: String.t()
+  @type woman() :: String.t()
+  @type men() :: %{man() => [woman()]}
+  @type women() :: %{woman() => [man()]}
+  @type couples() :: [{man(), woman()}]
 
-  def match(men, _women, state, true) do
-    state
-    |> Map.to_list()
-    |> Enum.slice(0..(length(Map.keys(men)) - 1))
+  @doc """
+  Stable match men and women using the Gale-Shapley algorithm.
+  """
+  @spec match(men(), women()) :: couples()
+  def match(men, women) do
+    score = calculate_score(women)
+    do_match(men, women, _state = %{}, score, _stable? = false)
   end
 
-  def match(men, women, state, false) do
-    {new_men, state} =
-      Enum.reduce(men, {men, state}, fn {m1, [f | preferences]}, {men, state} ->
-        is_m1_single = !state[m1]
-        is_f_single = !state[f]
-        m2 = if is_f_single, do: nil, else: state[f]
+  # `state` is a map holding all the couples
+  # if "a" and "z" are a couple
+  # then `state` will be equal to %{"a" => "z", "z" => "a"}
+  #
+  # `score` is used to quickly find out which man prefers the woman
+  #
+  # `stable?` stops the algorithm as soon as every man is paired to a woman
+  # it is done by checking that the number of keys in `state`
+  # is equal to two times the number of keys in `men` (one time for the men themselves and another time for the women)
+  defp do_match(men, _women, state, _score, true) do
+    men_keys = Map.keys(men)
+    for {k, v} <- state, k in men_keys, do: {k, v}
+  end
 
-        new_state =
-          case {is_m1_single, is_f_single, is_m1_prefered(women, f, m1, m2)} do
-            {true, true, true} -> state |> Map.put(m1, f) |> Map.put(f, m1)
-            {true, false, true} -> state |> Map.delete(m2) |> Map.put(m1, f) |> Map.put(f, m1)
-            _ -> state
+  defp do_match(men, women, state, score, false) do
+    {men, state} =
+      Enum.reduce(men, {men, state}, fn
+        {m1, [w | preferences]}, {men, state} ->
+          if m1_single? = !state[m1] do
+            w_single? = !state[w]
+            m2 = if w_single?, do: nil, else: state[w]
+            m1_prefered_to_m2? = if m2, do: score[w][m1] < score[w][m2], else: true
+
+            new_state =
+              case {m1_single?, w_single?, m1_prefered_to_m2?} do
+                {true, true, true} -> state |> Map.put(m1, w) |> Map.put(w, m1)
+                {true, false, true} -> state |> Map.delete(m2) |> Map.put(m1, w) |> Map.put(w, m1)
+                _ -> state
+              end
+
+            {_men = %{men | m1 => preferences}, new_state}
+          else
+            {men, state}
           end
-
-        {_men = %{men | m1 => preferences}, new_state}
       end)
 
-    match(new_men, women, state, is_stable(men, state))
+    do_match(men, women, state, score, stable?(men, state))
   end
 
-  def is_m1_prefered(_women, _woman, _m1, nil) do
-    true
+  defp calculate_score(women) do
+    for {w, prefs} <- women, into: %{} do
+      prefs_with_score =
+        prefs
+        |> Enum.with_index()
+        |> Enum.into(%{})
+
+      {w, prefs_with_score}
+    end
   end
 
-  def is_m1_prefered(women, woman, m1, m2) do
-    Enum.find_index(women[woman], &(&1 == m1)) <
-      Enum.find_index(women[woman], &(&1 == m2))
-  end
-
-  def is_stable(men, state) do
+  defp stable?(men, state) do
     length(Map.keys(men)) * 2 == length(Map.keys(state))
   end
 end
