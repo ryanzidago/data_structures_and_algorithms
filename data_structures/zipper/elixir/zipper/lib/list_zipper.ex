db3ed12d86c0da09e563347c1a5fa63516425287
Ryan Zidago <ryanzidago@gmail.com>
2021-10-12 22:53:58 +0200

    updated zipper project

---
+++ data_structures/zipper/elixir/zipper/lib/list_zipper.ex
@@ -0,0 +1,24 @@
+defmodule Zipper.ListZipper do
+  def new, do: {[], []}
+
+  def from_list(list) when is_list(list), do: {[], list}
+
+  def to_list({prev, next}), do: Enum.reverse(prev) ++ next
+
+  def prev({[], next}), do: {[], next}
+  def prev({[head | tail], next}), do: {tail, [head | next]}
+
+  def current({_, []}), do: nil
+  def current({_, [current | _]}), do: current
+
+  def next({prev, []}), do: {prev, []}
+  def next({prev, [head | tail]}), do: {[head | prev], tail}
+
+  def replace({prev, []}, val), do: {prev, [val]}
+  def replace({prev, [_ | next]}, val), do: {prev, [val | next]}
+
+  def insert({prev, next}, val), do: {prev, [val | next]}
+
+  def delete({prev, []}), do: {prev, []}
+  def delete({prev, [_ | next]}), do: {prev, next}
+end
