99de53450f954438c0eeebc2e192f6a35a5e4819
Ryan Zidago <ryanzidago@gmail.com>
2021-10-13 07:15:21 +0200

    added new function to binary tree in elixir

--- data_structures/binary_tree/elixir/binary_tree/lib/binary_tree.ex
+++ data_structures/binary_tree/elixir/binary_tree/lib/binary_tree.ex
@@ -1,6 +1,10 @@
 defmodule BinaryTree do
   defstruct [:val, :left, :right]
 
+  def new(val, left \\ nil, right \\ nil) do
+    %__MODULE__{val: val, left: left, right: right}
+  end
+
   def invert(nil), do: nil
 
   def invert(%__MODULE__{left: left, right: right} = node) do
--- data_structures/binary_tree/elixir/binary_tree/test/binary_tree_test.exs
+++ data_structures/binary_tree/elixir/binary_tree/test/binary_tree_test.exs
@@ -52,6 +52,18 @@ defmodule BinaryTreeTest do
     }
   end
 
+  describe "new/{1,2,3}" do
+    test "creates a new BinaryTree node" do
+      assert %BinaryTree{val: 1} == BinaryTree.new(1)
+
+      assert %BinaryTree{val: 1, left: %BinaryTree{val: 10}} ==
+               BinaryTree.new(1, BinaryTree.new(10))
+
+      assert %BinaryTree{val: 1, left: %BinaryTree{val: 10}, right: %BinaryTree{val: 100}} ==
+               BinaryTree.new(1, BinaryTree.new(10), BinaryTree.new(100))
+    end
+  end
+
   describe "symmetric?/1" do
     test "returns `true` if the BinaryTree is symmetric", %{symmetric_tree: symmetric_tree} do
       assert BinaryTree.symmetric?(symmetric_tree)
