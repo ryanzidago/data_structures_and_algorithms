a6a831d9d4b1ab4d14b857d8f7032146bc4dd932
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2021-10-13 20:52:00 +0200

    Update mix.exs

--- data_structures/zipper/elixir/zipper/mix.exs
+++ data_structures/zipper/elixir/zipper/mix.exs
@@ -22,8 +22,6 @@ defmodule Zipper.MixProject do
   defp deps do
     [
       {:binary_tree, path: "../../../binary_tree/elixir/binary_tree"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
